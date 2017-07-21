5275689e80763eff5cef014ecbcde319202a76e0
DUDULUU-PC\duduluu <duduluu@outlook.com>
2017-07-21 12:20:58 +0800

    Start project

--- RimWorld-Huiji-Exporter/Main.cs
+++ RimWorld-Huiji-Exporter/Main.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
 
-namespace RimWorld_Huiji_Exporter
-{
-    public class Main
-    {
+namespace RimWorld_Huiji_Exporter {
+    [StaticConstructorOnStartup]
+    public class Main {
+        static Main() {
+            Log.Message("[RimWorld-Huiji-Exporter]");
+        }
     }
 }
