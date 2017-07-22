e380de3f92115272464c4071e8d0a8617d823e31
DUDULUU-PC\duduluu <duduluu@outlook.com>
2017-07-22 15:18:13 +0800

    Add TextureUtility

---
+++ HuijiExporter/Utility/TextureUtility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuijiExporter.Utility {
+    public static class TextureUtility {
+        public static string PathToFile(string path) {
+            return $"Texture_{path.ToLower().Replace('/', '_')}.png";
+        }
+    }
+}
