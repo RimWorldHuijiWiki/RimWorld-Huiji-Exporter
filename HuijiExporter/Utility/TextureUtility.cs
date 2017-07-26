ec7f64e33332d6efd61257e40ab5dd63cc6c49b8
DUDULUU-PC\duduluu <duduluu@outlook.com>
2017-07-26 15:09:34 +0800

    Fix: Textures

--- HuijiExporter/Utility/TextureUtility.cs
+++ HuijiExporter/Utility/TextureUtility.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace HuijiExporter.Utility {
     public static class TextureUtility {
         public static string PathToFile(string path) {
-            return $"Texture_{path.ToLower().Replace('/', '_')}.png";
+            return $"Textures_{path.ToLower().Replace('/', '_')}.png";
         }
     }
 }
